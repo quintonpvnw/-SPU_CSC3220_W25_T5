4f4536d2997bd37a2357ec2d53819661b1aab774
Quinton Pho <quintonpho@gmail.com>
2025-02-21 21:41:18 -0800

    Final SQL Schema

---
+++ Database/BookMarked DB FINAL.sql
@@ -0,0 +1,68 @@
+--
+-- File generated with SQLiteStudio v3.4.15 on Fri Feb 21 21:40:51 2025
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: Author
+DROP TABLE IF EXISTS Author;
+
+CREATE TABLE IF NOT EXISTS Author (
+    AuthorID  INTEGER PRIMARY KEY AUTOINCREMENT,
+    FirstName TEXT    NOT NULL,
+    LastName  TEXT    NOT NULL,
+    Birthyear TEXT,
+    Biography TEXT
+)
+STRICT;
+
+
+-- Table: Book
+DROP TABLE IF EXISTS Book;
+
+CREATE TABLE IF NOT EXISTS Book (
+    BookID    INTEGER PRIMARY KEY AUTOINCREMENT,
+    Title     TEXT    NOT NULL,
+    Genre     TEXT,
+    Published TEXT,
+    IBSN      INTEGER UNIQUE,
+    Rating    INTEGER,
+    Review    TEXT,
+    Status    INTEGER NOT NULL
+                      AS (0),
+    Goal      TEXT
+)
+STRICT;
+
+
+-- Table: Wishlist
+DROP TABLE IF EXISTS Wishlist;
+
+CREATE TABLE IF NOT EXISTS Wishlist (
+    WishlistID INTEGER PRIMARY KEY AUTOINCREMENT,
+    BookID     ANY     REFERENCES Book (BookID),
+    Added_Date TEXT
+)
+STRICT;
+
+
+-- Table: WrittenBy
+DROP TABLE IF EXISTS WrittenBy;
+
+CREATE TABLE IF NOT EXISTS WrittenBy (
+    AuthorID INTEGER REFERENCES Author (AuthorID) 
+                     NOT NULL,
+    BookID   INTEGER REFERENCES Book (BookID) 
+                     NOT NULL,
+    PRIMARY KEY (
+        AuthorID,
+        BookID
+    )
+)
+STRICT;
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
